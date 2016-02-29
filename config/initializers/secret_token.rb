e945402a5bb65715a70fdc8b56ac3f6a365e0780
Wil Santana <santana.wil@gmail.com>
2016-02-29 12:21:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomster::Application.config.secret_key_base = '9b00a813963abe7fe687daceef2a5080374d68ee7bfd3e01366cffd26f58c26f1417a538938b7a824b20bd2baba515cdea75a900990f4185c41fd1d2ea79e77b'
