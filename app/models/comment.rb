656dea6fe34fa4ddf3c668107d53177c48538bc5
Wil Santana <santana.wil@gmail.com>
2016-03-10 12:46:22 +0000

    Form moved to modal format

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'one star'      => '*',
+        'two stars'     => '**',
+        'three stars'   => '***',
+        'four stars'    => '****',
+        'five stars'    => '*****'
+        }
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end
