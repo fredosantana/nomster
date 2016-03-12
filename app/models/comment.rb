4f4577d132102eeaa35fb3a37641914140bc1fe6
Wil Santana <santana.wil@gmail.com>
2016-03-12 16:57:13 +0000

    Migration error appears when re-running comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,2 @@
 class Comment < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :place
-    
-    RATINGS = {
-        'one star'      => '*',
-        'two stars'     => '**',
-        'three stars'   => '***',
-        'four stars'    => '****',
-        'five stars'    => '*****'
-        }
-    
-    def humanized_rating
-        RATINGS.invert[self.rating]
-    end
 end
